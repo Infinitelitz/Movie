d60add4cde5d1b9669db83994444cd5180fcd48f
Anbor <seisdub@gmail.com>
2024-02-12 01:55:42 -0300

    config

--- src/main/java/com/devsuperior/movieflix/config/AppConfig.java
+++ src/main/java/com/devsuperior/movieflix/config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.devsuperior.movieflix.components;
+package com.devsuperior.movieflix.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/devsuperior/movieflix/config/WebSecurityConfig.java
+++ src/main/java/com/devsuperior/movieflix/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.devsuperior.movieflix.components;
+package com.devsuperior.movieflix.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
